de9bcf1f87ac5705024459dc0678ae70faf65c63
muzo <151652470+muzo62@users.noreply.github.com>
2025-03-25 13:21:35 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// js/script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Mobile-Menü-Toggle
+  const menuToggle = document.querySelector('.mobile-menu-toggle');
+  const navLinks = document.querySelector('nav ul');
+
+  menuToggle.addEventListener('click', function() {
+    navLinks.classList.toggle('active');
+  });
+});
+
+// Elemente abrufen
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-image");
+const captionText = document.getElementById("caption");
+const closeBtn = document.getElementsByClassName("close")[0];
+const images = document.querySelectorAll(".gallery-item img");
+
+// Klick-Event für jedes Bild
+images.forEach((img) => {
+  img.addEventListener("click", function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;       // Quelle des angeklickten Bildes übernehmen
+    captionText.innerHTML = this.alt; // Alt-Text als Bildbeschreibung
+  });
+});
+
+// Schließen des Modals
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+window.addEventListener('scroll', function() {
+  const header = document.querySelector('header');
+  if (window.scrollY > 50) { // ab 50px Scrolltiefe
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
